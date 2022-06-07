a065c3ce2284e6bce58783d3c6a74b131c8cd2fc
eunchong2lee <tlfqjrjs1234@naver.com>
2022-06-07 10:04:51 +0800

    0607 문제 풀이

---
+++ Programmers/060722_1.js
@@ -0,0 +1,22 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12899
+// 124의 나라
+
+function solution(n) {
+  var answer = '';
+  const array = ['4','1','2']
+  while (n>0){
+      const remain = n % 3
+      answer = array[remain] + answer;
+      if(remain === 0){
+          n = Math.floor((n-1)/3)
+      }else{
+          n = Math.floor(n/3)
+      }
+  }
+  return answer;
+}
+
+n = 513
+
+a= n.toString(3)
+solution(n);
